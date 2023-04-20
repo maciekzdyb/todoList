509fa8e477b2d4e1692fcd0d8f7c287b6c73e1ab
maciekzdyb <maciekzdyb@gmail.com>
2023-04-20 14:44:20 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  config: { [key: string]: string | Date };
+
+  constructor(){
+    this.config = {};
+    setTimeout( () =>{
+      this.config = {
+        title: 'Lista zadań',
+        footer: ' © Lista zadań,All rights reserved.',
+        date: new Date()
+      }
+    },500);
+    
+    
+  }
+  
+
+}
